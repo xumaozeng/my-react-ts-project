2a70d8d6b362b2c8ac81a5bb1443a148a2abce44
xumaozeng <15611895341@163.com>
2022-07-21 22:08:05 +0800

    更新readme文档

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ src
     pages
         todoList  //这是一个完整的代办事项
         Axios.tsx //axios请求与代理的使用
+        githubSearch //这是一个github搜索案例
     ...
 ```
 
@@ -85,3 +86,38 @@ src
    1. 优点：可以配置多个代理，灵活的控制请求是否走代理
    2. 缺点：配置繁琐，前端请求资源时必须加前缀
 
+#### github搜索案例
+
+1. 设计状态时要考虑全面，例如带有网络请求的组件，要考虑请求失败怎么办
+
+2. ES6小知识点：解构赋值+重命名
+
+   ```
+   let obj = {a:{b:1}};
+   const {a} = obj; //传统解构赋值
+   const {a:{b}} = obj; //连续解构赋值
+   const {a:{b:value}} = obj; //连续解构赋值+重命名
+   ```
+
+3. 消息订阅与发布机制
+
+   ```
+   (1) 先订阅，再发布（理解：有一种隔空对话的感觉）
+   (2) 适用于任意组件间通信
+   (3) 要在组件的卸载生命钩子中取消订阅
+   ```
+
+4. fetch发送请求（关注分离的设计思想）
+
+   ```
+   try {
+     const response = await fetch(`/api/search/users?q=${keyWord}`);
+     const data = await response.json();
+     console.log(data);
+   } catch (error) {
+     console.log("请求错误", error);
+   }
+   // 注意在外层加上async函数
+   ```
+
+   
--- src/pages/githubSearch/List/index.tsx
+++ src/pages/githubSearch/List/index.tsx
@@ -1,6 +1,38 @@
 /**
  * List组件
  */
+/* import React from "react";
+import { dataProps } from "../types";
+import "./index.scss";
+
+type ListProps = dataProps;
+const List: React.FC<ListProps> = props => {
+  const { users, isFirst, loading, err } = props;
+
+  return (
+    <div className="row">
+      {isFirst ? (
+        <h3>初始化状态</h3>
+      ) : loading ? (
+        <p>正在加载中。。。</p>
+      ) : err ? (
+        <p>{err.message}</p>
+      ) : (
+        users?.map(user => (
+          <div key={user.id} className="card">
+            <a href={user.html_url} target="_blank" rel="noreferrer">
+              <img alt="react" src={user.avatar_url} style={{ width: 100 }} />
+            </a>
+            <p className="card-text">{user.login}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+export default List; */
+
+// 订阅组件-pubsub
 import React, { useEffect, useState } from "react";
 import { dataProps } from "../types";
 import PubSub from "pubsub-js";
